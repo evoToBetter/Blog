b3ef40332285b9a7db46912f4507a97c8996b988
eilhixs <thomas.s.li@ericsson.com>
2019-07-23 11:46:08 +0800

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:evotobetter/Blog.git master:gh-pages
+git push -f git@stonelee:evotobetter/Blog.git master:gh-pages
 
 cd -
